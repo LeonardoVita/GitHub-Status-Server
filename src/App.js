50a8fbb70076d95a11fcbb09664b6e760447ec6e
Leonardo Vita Milanez de Almeida <Leonardo.vita0606@gmail.com>
2020-10-28 19:47:35 -0300

    novo loading

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import cheerio from 'cheerio'
 import { FaRedo } from 'react-icons/fa'
+import cheerio from 'cheerio'
 
 import './global.css'
 import './index.css'
 
 function App() {
 
-  const [statusArray, setStatusArray] = useState([]);
+  const [statusArray, setStatusArray] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-  function getData() {
-    setStatusArray([])
-    axios.get('https://www.githubstatus.com/').then((res) => {
+  async function getData() {
+    
+    try {
+      setLoading(true)
+      
+      const res = await axios.get('https://www.githubstatus.com/')      
       const $ = cheerio.load(res.data)
-      let obj = []
-      let statusClass
+  
+      let webData = []
+      
       $(".component-container").each((i, element) => {
-
+  
         const name = $(element)
           .find("span.name")
           .text()
@@ -26,48 +31,74 @@ function App() {
           .find("span.component-status")
           .text()
           .trim()
-
-        statusClass = (status === "Operational") ? "normal" : "inTroble"
+  
+        const statusClass = (status === "Operational") ? "normal" : "inTroble"
         status = (status === "Operational") ? "Normal" : "Operational"
-
+  
         if (!(name === "Visit www.githubstatus.com for more information") && !(name === "Other"))
-          obj = [...obj, { name, status, statusClass }]
-
+          webData = [...webData, { name, status, statusClass }]
+  
       })
-
-      setStatusArray(obj)
-    })
+      setStatusArray(webData)  
+      
+    } catch (error) {
+      console.error(error)
+    }finally{
+      setLoading(false)
+    }
+    
   }
 
   useEffect(() => {
     getData()
   }, [])
-
+ 
   return (
-    <div className="container wrap">
-      <h1>Status do gitHub server</h1>
-      <ul>
-        {statusArray.map(item => (
-          <li key={item.name} className="status-container grid-4">
-            <h2>{item.name}</h2>
-            <span className={item.statusClass}>{item.status}</span>
-          </li>
-        ))}
-        <li className="status-container grid-4 reload" onClick={getData}>
-          <FaRedo size="50%" />
-        </li>
-        <li className="status-container grid-4 reload-mobile" onClick={getData}>
-          <p>Verificar Status</p>
-          <FaRedo size="16px" />
-        </li>
-      </ul>
-      <div className="disclaimer">
-        <p>Este um app não oficial, para mais informações oficias utiliza o link da pagina oficial do
-          <a href="https://www.githubstatus.com" target="_blank" rel="noopener noreferrer"> githubstatus.com</a></p>
-        <p>Este é um Projeto feito por Leonardo vita, visite o meu Repositorio no github <span role="img" arial-label="foguete">🚀</span>
-          <a href="https://github.com/LeonardoVita" target="_blank" rel="noopener noreferrer"> Meu Repositorio</a></p>
+    <>
+      {
+        loading && (
+          <div className="loading-container">
+          <div className="loading"/>
+          </div>
+        ) 
+          
+      }
+      <div className="container wrap">
+        <div className="content">
+          <div className="title-container">
+            <h1>Status do gitHub server</h1> 
+            <div className="reload-button" onClick={getData}>
+              <FaRedo  size="2rem" color="teal"/>
+            </div>
+          </div>         
+          <ul>
+            {
+              statusArray && 
+              statusArray.map(item => (
+                <li key={item.name} className="status-container grid-4">
+                  <h2>{item.name}</h2>
+                  <span className={item.statusClass}>{item.status}</span>
+                </li>
+              ))
+            }
+            {
+              statusArray && 
+              <li className="status-container grid-4 reload-mobile" onClick={getData}>
+                <p>Verificar Status</p>
+                <FaRedo size="16px" />
+              </li>
+            }
+            
+          </ul>
+        </div>
+        <div className="disclaimer">
+          <p>Este um app não oficial, para mais informações oficias utiliza o link da pagina oficial do
+            <a href="https://www.githubstatus.com" target="_blank" rel="noopener noreferrer"> githubstatus.com</a></p>
+          <p>Este é um Projeto feito por Leonardo vita, visite o meu Repositorio no github <span role="img" arial-label="foguete">🚀</span>
+            <a href="https://github.com/LeonardoVita" target="_blank" rel="noopener noreferrer"> Meu Repositorio</a></p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
